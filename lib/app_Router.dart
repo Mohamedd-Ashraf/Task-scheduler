de162157a064c9909b84b86fa1a51544ace1b4b6
Mohamedd-Ashraf <110347603+Mohamedd-Ashraf@users.noreply.github.com>
2022-08-08 00:00:06 +0200

    UI

--- lib/app_Router.dart
+++ lib/app_Router.dart
@@ -1,22 +1,37 @@
 // import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Business_Logic/cubit/chracters_cubit.dart';
 import 'package:flutter_application_1/Constants/Strings.dart';
 import 'package:flutter_application_1/data/API/web_services_page.dart';
 import 'package:flutter_application_1/data/Repo/chracter_repo.dart';
 import 'package:flutter_application_1/interface/screens/chracter_page_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'Constants/Strings.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
+    late ChractersRepo chractersRepo;
+    late ChractersCubit chractersCubit;
+
+    AppRouter() {
+      chractersRepo = ChractersRepo(WebServices());
+      chractersCubit = ChractersCubit(chractersRepo);
+    }
+
     switch (settings.name) {
       case allChracterScreen:
-        return MaterialPageRoute(builder: (_) => ChracterScreen());
-        break;
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (BuildContext context) => chractersCubit,
+            child: ChracterScreen(),
+          ),
+        );
+      
 
       case detialsScreen:
         return MaterialPageRoute(builder: (_) => ChracterScreen());
-        break;
+        
 
       default:
     }
--- lib/interface/screens/chracter_page_screen.dart
+++ lib/interface/screens/chracter_page_screen.dart
@@ -1,10 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Business_Logic/cubit/chracters_cubit.dart';
+import 'package:flutter_application_1/Constants/MyColors.dart';
+import 'package:flutter_application_1/data/models/chracters.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ChracterScreen extends StatelessWidget {
+late List<Chracter> allChracter;
+
+class ChracterScreen extends StatefulWidget {
   const ChracterScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return (Container(child: Text("saasd")));
+  State<ChracterScreen> createState() => _ChracterScreenState();
+}
+
+class _ChracterScreenState extends State<ChracterScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    allChracter = BlocProvider.of<ChractersCubit>(context).getAllCharacters();
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All Chracters"),
+        backgroundColor: MyColors.MyYellow,
+      ),
+      body: buildBlockWidget(),
+    );
+  }
+}
+
+Widget buildBlockWidget() {
+  return BlocBuilder<ChractersCubit, ChractersState>(
+      builder: ((context, state) {
+    if (state is ChractersIsLoaded) {
+      allChracter = (state).chractersList;
+      return buildLoadListWidget();
+    } else {
+      return screenIdacator();
+    }
+  }));
+}
+
+Widget screenIdacator() {
+  return Center();
+}
+
+Widget buildLoadListWidget() {
+  return SingleChildScrollView(
+    child: Container(
+      color: MyColors.Mygrey,
+      child: Column(
+        children: [buildCharcterList()],
+      ),
+    ),
+  );
+}
+
+Widget buildCharcterList() {
+  return GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+          childAspectRatio: 2/3,
+          crossAxisSpacing: 1,
+          mainAxisSpacing: 1 ,
+          ),
+      itemBuilder: ((context, index) {
+       retuen CharacterItem();
+      }));
 }
